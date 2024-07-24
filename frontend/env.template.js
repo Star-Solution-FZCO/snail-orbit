af026a9a6717ffa35bfdfa08583d61e01eb90a8e
alikhvn <theololo057@gmail.com>
2024-07-24 22:25:34 +0500

    added frontend app project template

---
+++ frontend/env.template.js
@@ -0,0 +1,7 @@
+(function (window) {
+    window["env"] = window["env"] || {};
+
+    window["env"]["API_BASE_URL"] = "${API_BASE_URL}";
+    window["env"]["MUI_X_LICENSE_KEY"] = "${MUI_X_LICENSE_KEY}";
+    window["env"]["STORAGE_URL"] = "${STORAGE_URL}";
+})(this);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>Project Manager</div>;
+};
+
+export default App;
