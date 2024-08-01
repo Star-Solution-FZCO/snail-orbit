d68c89d17d4858bbbcb5efb1e4cd7325cacc3a8a
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2024-08-02 01:30:01 +0200

    back: db audit

--- pm/models/issue.py
+++ pm/models/issue.py
@@ -1,6 +1,7 @@
 from beanie import Document, Indexed
 from pydantic import BaseModel, Extra, Field
 
+from ._audit import audited_model
 from .project import ProjectLinkField
 from .user import UserLinkField
 
@@ -15,11 +16,13 @@ class IssueComment(BaseModel):
     text: str | None
 
 
+@audited_model
 class Issue(Document):
     class Settings:
         name = 'issues'
         use_revision = True
         use_state_management = True
+        state_management_save_previous = True
 
     class Config:
         extra = Extra.allow
--- pm/models/project.py
+++ pm/models/project.py
@@ -1,19 +1,23 @@
 from beanie import Document, Indexed, PydanticObjectId
 from pydantic import BaseModel
 
+from ._audit import audited_model
+
 __all__ = (
     'Project',
     'ProjectLinkField',
 )
 
 
+@audited_model
 class Project(Document):
     class Settings:
         name = 'projects'
         use_revision = True
         use_state_management = True
+        state_management_save_previous = True
 
-    name: Indexed(str)
+    name: str = Indexed(str)
     slug: str = Indexed(str, unique=True)
     description: str | None = None
     is_active: bool = True
