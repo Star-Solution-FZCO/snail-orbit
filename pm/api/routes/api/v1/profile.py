29205b4f6c1c2ebe9bec3091b454dd544870fe54
BelCo <nya.belco@gmail.com>
2024-08-02 20:41:07 +0200

    back: profile endpoint

---
+++ pm/api/routes/api/v1/profile.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends
+
+import pm.models as m
+from pm.api.context import current_user, current_user_context_dependency
+from pm.api.views.factories.crud import CrudOutput
+from pm.api.views.output import SuccessPayloadOutput
+
+__all__ = ('router',)
+
+router = APIRouter(
+    prefix='/profile',
+    tags=['profile'],
+    dependencies=Depends(current_user_context_dependency),
+)
+
+
+class Profile(CrudOutput[m.User]):
+    name: str
+    email: str
+    is_active: bool
+    is_admin: bool
+
+
+@router.get('/')
+async def add_token() -> SuccessPayloadOutput[Profile]:
+    user = current_user()
+    return SuccessPayloadOutput(payload=Profile.from_obj(user))
