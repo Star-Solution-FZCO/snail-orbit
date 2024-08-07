2984d7f3666ae742453ec8daa246ae8e98e6b686
5kif4a <theololo057@gmail.com>
2024-08-07 15:14:43 +0500

    local environment settings with cookies to work in google chrome

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,4 @@
 export const API_URL =
-    (window as any)?.env?.API_URL ||
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:9090/api/";
+    (window as any)?.env?.API_URL || import.meta.env.VITE_API_URL || "/api/";
 
 export const apiVersion = "v1";
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -8,9 +9,17 @@ export default defineConfig({
     server: {
         host: "localhost",
         port: 3000,
+        open: true,
+        https: true,
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:9090",
+                changeOrigin: true,
+            },
+        },
     },
     build: {
         outDir: "./build",
     },
-    plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
+    plugins: [react(), tsconfigPaths(), TanStackRouterVite(), basicSsl()],
 });
