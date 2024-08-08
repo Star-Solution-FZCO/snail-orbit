146d83481805b8dd5262b82af7a8e37c9d48addc
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2024-08-08 17:08:11 +0200

    back: issues custom fields

--- pm/api/routes/api/v1/issue.py
+++ pm/api/routes/api/v1/issue.py
@@ -1,9 +1,9 @@
 from http import HTTPStatus
-from typing import Self
+from typing import Any, Self
 
 from beanie import PydanticObjectId
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 import pm.models as m
 from pm.api.context import current_user_context_dependency
@@ -53,6 +53,7 @@ class IssueOutput(BaseModel):
     subject: str
     text: str | None
     comments: list[IssueCommentOutput]
+    fields: dict[str, m.CustomFieldValue]
 
     @classmethod
     def from_obj(cls, obj: m.Issue) -> Self:
@@ -62,6 +63,7 @@ class IssueOutput(BaseModel):
             subject=obj.subject,
             text=obj.text,
             comments=[IssueCommentOutput.from_obj(comment) for comment in obj.comments],
+            fields=obj.fields,
         )
 
 
@@ -69,11 +71,13 @@ class IssueCreate(BaseModel):
     project_id: PydanticObjectId
     subject: str
     text: str | None = None
+    fields: dict[str, Any] = Field(default_factory=dict)
 
 
 class IssueUpdate(BaseModel):
     subject: str | None = None
     text: str | None = None
+    fields: dict[str, Any] | None = None
 
 
 @router.get('/list')
@@ -106,13 +110,17 @@ async def get_issue(
 async def create_issue(
     body: IssueCreate,
 ) -> ModelIdOutput:
-    project = await m.Project.find_one(m.Project.id == body.project_id)
+    project: m.Project | None = await m.Project.find_one(
+        m.Project.id == body.project_id, fetch_links=True
+    )
     if not project:
         raise HTTPException(HTTPStatus.BAD_REQUEST, 'Project not found')
+    validated_fields = await validate_custom_fields_values(body.fields, project)
     obj = m.Issue(
         subject=body.subject,
         text=body.text,
         project=m.ProjectLinkField(id=project.id, name=project.name, slug=project.slug),
+        fields=validated_fields,
     )
     await obj.insert()
     return ModelIdOutput.from_obj(obj)
@@ -123,11 +131,44 @@ async def update_issue(
     issue_id: PydanticObjectId,
     body: IssueUpdate,
 ) -> ModelIdOutput:
-    obj = await m.Issue.find_one(m.Issue.id == issue_id)
+    obj: m.Issue | None = await m.Issue.find_one(
+        m.Issue.id == issue_id, fetch_links=True
+    )
     if not obj:
         raise HTTPException(HTTPStatus.NOT_FOUND, 'Issue not found')
     for k, v in body.dict(exclude_unset=True).items():
+        if k == 'fields':
+            project = await obj.get_project(fetch_links=True)
+            v = await validate_custom_fields_values(v, project, obj)
+            obj.fields.update(v)
+            continue
         setattr(obj, k, v)
     if obj.is_changed:
         await obj.save_changes()
     return ModelIdOutput.from_obj(obj)
+
+
+async def validate_custom_fields_values(
+    fields: dict[str, Any], project: m.Project, issue: m.Issue | None = None
+) -> dict[str, m.CustomFieldValue]:
+    all_issue_fields = set(fields.keys())
+    if issue:
+        all_issue_fields |= set(issue.fields.keys())
+    for f in project.custom_fields:  # type: m.CustomField
+        if f.is_nullable:
+            continue
+        if f.name not in all_issue_fields:
+            raise HTTPException(HTTPStatus.BAD_REQUEST, f'Field {f.name} is required')
+
+    results = {}
+    project_fields = {f.name: f for f in project.custom_fields}
+    for key, val in fields.items():
+        if key not in project_fields:
+            raise HTTPException(HTTPStatus.BAD_REQUEST, f'Field {key} is not allowed')
+        val = project_fields[key].validate_value(val)
+        results[key] = m.CustomFieldValue(
+            id=project_fields[key].id,
+            type=project_fields[key].type,
+            value=val,
+        )
+    return results
--- pm/models/custom_fields.py
+++ pm/models/custom_fields.py
@@ -1,9 +1,10 @@
+from datetime import date, datetime
 from enum import StrEnum
+from typing import Any
 from uuid import UUID
 
-from beanie import Document
+from beanie import Document, PydanticObjectId
 from pydantic import BaseModel, Field
-from pydantic.color import Color
 
 from ._audit import audited_model
 
@@ -21,6 +22,7 @@ __all__ = (
     'UserMultiCustomField',
     'EnumCustomField',
     'EnumMultiCustomField',
+    'CustomFieldValue',
 )
 
 
@@ -58,47 +60,143 @@ class CustomField(Document):
     type: CustomFieldTypeT
     is_nullable: bool
 
+    def validate_value(self, value: Any) -> Any:
+        if value is None and not self.is_nullable:
+            raise ValueError(f'Field {self.name} cannot be null')
+        return value
+
 
 class StringCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.STRING
+
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is not None and not isinstance(value, str):
+            raise ValueError(f'Field {self.name} must be a string')
+        return value
 
 
 class IntegerCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.INTEGER
 
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is not None and not isinstance(value, int):
+            raise ValueError(f'Field {self.name} must be an integer')
+        return value
+
 
 class FloatCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.FLOAT
+
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is not None and not isinstance(value, float):
+            raise ValueError(f'Field {self.name} must be a float')
+        return value
 
 
 class BooleanCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.BOOLEAN
 
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is not None and not isinstance(value, bool):
+            raise ValueError(f'Field {self.name} must be a boolean')
+        return value
+
 
 class DateCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.DATE
+
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is None:
+            return value
+        if isinstance(value, date):
+            return value
+        if isinstance(value, str):
+            try:
+                return datetime.strptime(value, '%Y-%m-%d').date()
+            except ValueError as err:
+                raise ValueError(
+                    f'Field {self.name} has wrong date format, must be YYYY-MM-DD'
+                ) from err
+        raise ValueError(f'Field {self.name} must be a date in ISO format')
 
 
 class DateTimeCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.DATETIME
 
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is None:
+            return value
+        if isinstance(value, datetime):
+            return value
+        if isinstance(value, str):
+            try:
+                return datetime.fromisoformat(value)
+            except ValueError as err:
+                raise ValueError(
+                    f'Field {self.name} has wrong datetime format, must be ISO format'
+                ) from err
+        raise ValueError(f'Field {self.name} must be a datetime in ISO format')
+
 
 class UserCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.USER
 
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        # todo: validate user
+        return value
+
 
 class UserMultiCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.USER_MULTI
+
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is None:
+            return value
+        if not isinstance(value, list):
+            raise ValueError(f'Field {self.name} must be a list')
+        if not self.is_nullable and not value:
+            raise ValueError(f'Field {self.name} cannot be empty')
+        return value
 
 
 class EnumCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.ENUM
     options: dict[UUID, EnumOption] = Field(default_factory=dict)
 
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is None:
+            return value
+        if value not in {opt.value for opt in self.options.values()}:
+            raise ValueError(f'Field {self.name} has wrong value')
+        return value
+
 
 class EnumMultiCustomField(CustomField):
     type: CustomFieldTypeT = CustomFieldTypeT.ENUM_MULTI
     options: dict[UUID, EnumOption] = Field(default_factory=dict)
+
+    def validate_value(self, value: Any) -> Any:
+        value = super().validate_value(value)
+        if value is None:
+            return value
+        if not isinstance(value, list):
+            raise ValueError(f'Field {self.name} must be a list')
+        if not self.is_nullable and not value:
+            raise ValueError(f'Field {self.name} cannot be empty')
+        allowed_values = {opt.value for opt in self.options.values()}
+        for val in value:
+            if val not in allowed_values:
+                raise ValueError(f'Field {self.name} has wrong value "{val}"')
+        return value
 
 
 MAPPING = {
@@ -113,3 +211,9 @@ MAPPING = {
     CustomFieldTypeT.ENUM: EnumCustomField,
     CustomFieldTypeT.ENUM_MULTI: EnumMultiCustomField,
 }
+
+
+class CustomFieldValue(BaseModel):
+    id: PydanticObjectId
+    type: CustomFieldTypeT
+    value: Any
