4f51e626fde51bb1890a67a78a763cefae761e21
BelCo <nya.belco@gmail.com>
2024-08-16 01:01:12 +0200

    back: add user groups

--- pm/models/__init__.py
+++ pm/models/__init__.py
@@ -1,12 +1,14 @@
 from ._audit import *
 from .board import *
 from .custom_fields import *
+from .group import *
 from .issue import *
 from .project import *
 from .user import *
 
 __beanie_models__ = [
     AuditRecord,
+    Group,
     User,
     Project,
     Issue,
