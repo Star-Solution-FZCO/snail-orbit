eaed0d878844a39d525c39c821df03c1d7bb34be
5kif4a <theololo057@gmail.com>
2024-09-05 01:04:17 +0500

    feat: Add sign in with Workbench option in authentication module

--- frontend/src/utils/error.ts
+++ frontend/src/utils/error.ts
@@ -2,7 +2,6 @@ import i18n from "i18n";
 import { toast } from "react-toastify";
 
 export const toastApiError = (error: any) => {
-    toast.error(
-        error?.data?.error_messages?.join(". ") || i18n.t("error.default"),
-    );
+    const errorMessages = error?.error_messages || error?.data?.error_messages;
+    toast.error(errorMessages?.join(". ") || i18n.t("error.default"));
 };
