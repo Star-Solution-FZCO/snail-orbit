a3b2c65a6ccf4120f91aff86d30002fa702473c4
BelCo <nya.belco@gmail.com>
2024-09-11 01:21:46 +0200

    back: add avatar links to the user output

--- pm/api/views/user.py
+++ pm/api/views/user.py
@@ -1,7 +1,10 @@
 from typing import TYPE_CHECKING, Self
+from urllib.parse import quote
 
 from beanie import PydanticObjectId
 from pydantic import BaseModel
+
+from pm.constants import AVATAR_SIZES
 
 if TYPE_CHECKING:
     import pm.models as m
@@ -9,11 +12,14 @@ if TYPE_CHECKING:
 
 __all__ = ('UserOutput',)
 
+AVATAR_URL = '/api/avatar?email={email}&size={size}'
+
 
 class UserOutput(BaseModel):
     id: PydanticObjectId
     name: str
     email: str
+    avatars: dict[int, str]
 
     @classmethod
     def from_obj(cls, obj: 'm.User | m.UserLinkField') -> Self:
@@ -21,4 +27,8 @@ class UserOutput(BaseModel):
             id=obj.id,
             name=obj.name,
             email=obj.email,
+            avatars={
+                size: AVATAR_URL.format(email=quote(obj.email), size=size)
+                for size in AVATAR_SIZES
+            },
         )
