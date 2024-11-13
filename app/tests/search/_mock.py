8f8ce4616d080e332b3d3ecd7b4db352d7e288b4
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2024-11-14 00:04:53 +0100

    Add parameterized tests and query transformation logic
    This commit introduces new parameterized tests for search suggestions and query transformations.

--- app/tests/search/_mock.py
+++ app/tests/search/_mock.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
 
 from pm.models import CustomFieldTypeT
 
@@ -8,15 +8,56 @@ __all__ = (
 )
 
 
-@dataclass
-class FakeCustomField:
+class FakeCustomField(BaseModel):
     name: str
     type: CustomFieldTypeT
     is_nullable: bool
 
 
-def get_fake_custom_fields() -> dict[str, list[FakeCustomField]]:
-    return {
-        'State': [FakeCustomField('State', CustomFieldTypeT.STATE, True)],
-        'Priority': [FakeCustomField('Priority', CustomFieldTypeT.ENUM, True)],
-    }
+class FakeEnumField(BaseModel):
+    value: str
+
+
+class FakeStateField(BaseModel):
+    state: str
+
+
+class FakeEnumCustomField(FakeCustomField):
+    type: CustomFieldTypeT = CustomFieldTypeT.ENUM
+    options: list[FakeEnumField]
+
+
+class FakeStateCustomField(FakeCustomField):
+    type: CustomFieldTypeT = CustomFieldTypeT.STATE
+    options: list[FakeStateField]
+
+
+def parse_dict_to_field(field: dict) -> FakeCustomField:
+    if field['type'] == CustomFieldTypeT.ENUM:
+        return FakeEnumCustomField(
+            name=field['name'],
+            type=field['type'],
+            is_nullable=field['is_nullable'],
+            options=[FakeEnumField(value=option) for option in field['options']],
+        )
+    if field['type'] == CustomFieldTypeT.STATE:
+        return FakeStateCustomField(
+            name=field['name'],
+            type=field['type'],
+            is_nullable=field['is_nullable'],
+            options=[FakeStateField(state=option) for option in field['options']],
+        )
+    return FakeCustomField(
+        name=field['name'],
+        type=field['type'],
+        is_nullable=field['is_nullable'],
+    )
+
+
+def get_fake_custom_fields(
+    fields: list[dict],
+) -> dict[str, list[FakeCustomField]]:
+    res = {}
+    for field in fields:
+        res.setdefault(field['name'], []).append(parse_dict_to_field(field))
+    return res
