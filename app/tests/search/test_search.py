f27c200e842e6d6bdc6ada739dbb0c51b4703a9d
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2024-11-13 13:45:47 +0100

    search: refactor custom field handling
    Modify functions to pass cached_fields parameter instead of calling _get_custom_fields internally. This change optimizes the performance by reducing redundant asynchronous calls and allows better unit testing by mocking the custom fields. Adjust related tests to reflect this update.

--- app/tests/search/test_search.py
+++ app/tests/search/test_search.py
@@ -6,10 +6,11 @@ from ._mock import get_fake_custom_fields
 
 @mock.patch('pm.api.search.issue._get_custom_fields', new_callable=mock.AsyncMock)
 @pytest.mark.asyncio
-async def test_search(mock__get_custom_fields) -> None:
+async def test_search(mock__get_custom_fields: mock.AsyncMock) -> None:
     mock__get_custom_fields.return_value = get_fake_custom_fields()
 
     from pm.api.search.issue import get_suggestions
 
     res = await get_suggestions('State: open')
+    mock__get_custom_fields.assert_awaited_once()
     assert res == ['AND', 'OR']
