00a107e14ed020ebf49c547dc515128afb8d39fb
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2024-12-05 01:24:26 +0100

    tasks: add workflow scheduler

--- app/pm/tasks/actions/__init__.py
+++ app/pm/tasks/actions/__init__.py
@@ -1,7 +1,9 @@
 from .notify import task_notify_by_pararam
 from .send_email import task_send_email
+from .workflows import task_run_workflows
 
 __all__ = (
     'task_notify_by_pararam',
     'task_send_email',
+    'task_run_workflows',
 )
