2c3ad1379658e4667725b8ca453eba87f8c0e11b
polecie <13479460-polecie@users.noreply.gitlab.com>
2024-12-06 14:06:33 +0400

    refactor: imporve models, change routes and outputs

--- app/pm/models/search.py
+++ app/pm/models/search.py
@@ -1,5 +1,6 @@
 from enum import StrEnum
 from typing import Annotated
+from uuid import UUID, uuid4
 
 from beanie import Document, Indexed
 from pydantic import BaseModel, Field
@@ -8,16 +9,17 @@ from ._audit import audited_model
 from .group import GroupLinkField
 from .user import UserLinkField
 
-__all__ = ('Search', 'SearchShareType', 'SearchShare')
+__all__ = ('Search', 'SearchPermissionType', 'SearchPermission')
 
 
-class SearchShareType(StrEnum):
+class SearchPermissionType(StrEnum):
     GROUP = 'group'
     USER = 'user'
 
 
-class SearchShare(BaseModel):
-    target_type: SearchShareType
+class SearchPermission(BaseModel):
+    id: Annotated[UUID, Field(default_factory=uuid4)]
+    target_type: SearchPermissionType
     target: GroupLinkField | UserLinkField
 
 
@@ -32,4 +34,4 @@ class Search(Document):
     name: str = Indexed(str)
     query: str
     owner: UserLinkField
-    shared: Annotated[list[SearchShare], Field(default_factory=list)]
+    permissions: Annotated[list[SearchPermission], Field(default_factory=list)]
