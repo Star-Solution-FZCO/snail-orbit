e384dc8e580c00afb95aaf29845dc7af83b8412a
polecie <13479460-polecie@users.noreply.gitlab.com>
2024-12-06 14:06:33 +0400

    refactor: imporve models, change routes and outputs

--- app/pm/api/routes/api/v1/search.py
+++ app/pm/api/routes/api/v1/search.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 from typing import Self
+from uuid import UUID
 
 from beanie import PydanticObjectId
 from fastapi import Depends, HTTPException
@@ -27,9 +28,23 @@ router = APIRouter(
 )
 
 
-class SearchShareOutput(BaseModel):
-    target_type: m.SearchShareType
+class SearchPermissionOutput(BaseModel):
+    id: UUID
+    target_type: m.SearchPermissionType
     target: UserOutput | GroupOutput
+
+    @classmethod
+    def from_obj(cls, obj: m.SearchPermission) -> Self:
+        target = (
+            GroupOutput.from_obj(obj.target)
+            if obj.target_type == m.SearchPermissionType.GROUP
+            else UserOutput.from_obj(obj.target)
+        )
+        return cls(
+            id=obj.id,
+            target_type=obj.target_type,
+            target=target,
+        )
 
 
 class SearchOutput(BaseModel):
@@ -37,46 +52,36 @@ class SearchOutput(BaseModel):
     name: str
     query: str
     owner: UserOutput
-    shared: list[SearchShareOutput]
+    permissions: list[SearchPermissionOutput]
 
     @classmethod
     def from_obj(cls, obj: m.Search) -> Self:
         user_ctx = current_user()
-        results = []
-        user_groups = [g.id for g in user_ctx.user.groups]
-        for p in obj.shared:
-            match p.target_type:
-                case m.SearchShareType.GROUP:
-                    if p.target.id in user_groups:
-                        results.append(
-                            SearchShareOutput(
-                                target_type=p.target_type,
-                                target=GroupOutput.from_obj(p.target),
-                            )
-                        )
-                case m.SearchShareType.USER:
-                    results.append(
-                        SearchShareOutput(
-                            target_type=p.target_type,
-                            target=UserOutput.from_obj(p.target),
-                        )
-                    )
+        user_group_ids = {g.id for g in user_ctx.user.groups}
+        permissions = []
+        for permission in obj.permissions:
+            if permission.target_type == m.SearchPermissionType.GROUP:
+                if permission.target.id in user_group_ids:
+                    permissions.append(SearchPermissionOutput.from_obj(permission))
+            else:
+                permissions.append(SearchPermissionOutput.from_obj(permission))
+
         return cls(
             id=obj.id,
             name=obj.name,
             query=obj.query,
             owner=UserOutput.from_obj(obj.owner),
-            shared=results,
+            permissions=permissions,
         )
 
 
-class SearchShareBody(BaseModel):
-    target_type: m.SearchShareType
+class SearchPermissionBody(BaseModel):
+    target_type: m.SearchPermissionType
     target: PydanticObjectId
 
 
 class SearchCreate(BaseModel):
-    name: str | None = None
+    name: str
     query: str
 
 
@@ -90,17 +95,17 @@ async def list_searches(
         '$or': [
             {'owner.id': user_ctx.user.id},
             {
-                'shared': {
+                'permissions': {
                     '$elemMatch': {
-                        'target_type': m.SearchShareType.USER,
+                        'target_type': m.SearchPermissionType.USER,
                         'target.id': user_ctx.user.id,
                     }
                 }
             },
             {
-                'shared': {
+                'permissions': {
                     '$elemMatch': {
-                        'target_type': m.SearchShareType.GROUP,
+                        'target_type': m.SearchPermissionType.GROUP,
                         'target.id': {'$in': user_groups},
                     }
                 }
@@ -116,19 +121,17 @@ async def list_searches(
     )
 
 
-# pylint: disable=raise-missing-from
 @router.post('/')
 async def create_search(body: SearchCreate) -> SuccessPayloadOutput[SearchOutput]:
     user_ctx = current_user()
     try:
         await transform_query(body.query)
     except TransformError as err:
-        raise HTTPException(HTTPStatus.BAD_REQUEST, err.message)
+        raise HTTPException(HTTPStatus.BAD_REQUEST, err.message)  # pylint: disable=raise-missing-from
     except Exception as err:
-        raise HTTPException(HTTPStatus.BAD_REQUEST, err.args[0])
-    name = body.name if body.name else body.query
+        raise HTTPException(HTTPStatus.BAD_REQUEST, str(err)) from err
     search = m.Search(
-        name=name,
+        name=body.name,
         query=body.query,
         owner=m.UserLinkField.from_obj(user_ctx.user),
     )
@@ -148,9 +151,9 @@ async def delete_search(search_id: PydanticObjectId) -> ModelIdOutput:
     return ModelIdOutput.from_obj(search)
 
 
-@router.post('/{search_id}/share')
+@router.post('/{search_id}/permission')
 async def share_search(
-    search_id: PydanticObjectId, body: list[SearchShareBody]
+    search_id: PydanticObjectId, body: list[SearchPermissionBody]
 ) -> SuccessPayloadOutput[SearchOutput]:
     user_ctx = current_user()
     search = await m.Search.find_one(m.Search.id == search_id)
@@ -166,7 +169,7 @@ async def share_search(
     for p in body:
         if any(
             i.target_type == p.target_type and i.target.id == p.target
-            for i in search.shared
+            for i in search.permissions
         ):
             raise HTTPException(
                 status_code=HTTPStatus.CONFLICT,
@@ -175,7 +178,7 @@ async def share_search(
         if p.target in unique:
             continue
         unique.add(p.target)
-        if p.target_type == m.SearchShareType.USER:
+        if p.target_type == m.SearchPermissionType.USER:
             user = await m.User.find_one(m.User.id == p.target)
             if not user:
                 raise HTTPException(
@@ -188,12 +191,12 @@ async def share_search(
                     detail='Cannot add yourself to share list',
                 )
             results.append(
-                m.SearchShare(
+                m.SearchPermission(
                     target_type=p.target_type,
                     target=m.UserLinkField.from_obj(user),
                 )
             )
-        if p.target_type == m.SearchShareType.GROUP:
+        if p.target_type == m.SearchPermissionType.GROUP:
             group = await m.Group.find_one(m.Group.id == p.target)
             if not group:
                 raise HTTPException(
@@ -206,7 +209,7 @@ async def share_search(
                     detail='You can only share searches with groups you are a member of',
                 )
             results.append(
-                m.SearchShare(
+                m.SearchPermission(
                     target_type=p.target_type, target=m.GroupLinkField.from_obj(group)
                 )
             )
@@ -214,15 +217,15 @@ async def share_search(
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST, detail='No new valid share list to add'
         )
-    search.shared.extend(results)
+    search.permissions.extend(results)
     if search.is_changed:
         await search.save_changes()
     return SuccessPayloadOutput(payload=SearchOutput.from_obj(search))
 
 
-@router.delete('/{search_id}/unshare')
+@router.delete('/{search_id}/permission/{permission_id}')
 async def unshare_search(
-    search_id: PydanticObjectId, body: list[SearchShareBody]
+    search_id: PydanticObjectId, permission_id: UUID
 ) -> SuccessPayloadOutput[SearchOutput]:
     user_ctx = current_user()
     search = await m.Search.find_one(m.Search.id == search_id)
@@ -233,28 +236,17 @@ async def unshare_search(
             status_code=HTTPStatus.FORBIDDEN,
             detail='You cannot modify share list for this search',
         )
-    results = []
-    unique = set()
-    for p in body:
-        shared: None | m.SearchShare = next(
-            (
-                obj
-                for obj in search.shared
-                if obj.target_type == p.target_type and obj.target.id == p.target
-            ),
+    if not (
+        target := next(
+            (obj for obj in search.permissions if obj.id == permission_id),
             None,
         )
-        if not shared:
-            raise HTTPException(
-                status_code=HTTPStatus.CONFLICT,
-                detail=f'Search is not shared with {p.target_type} {p.target}',
-            )
-        if shared.target.id in unique:
-            continue
-        unique.add(shared.target.id)
-        results.append(shared)
-    for r in results:
-        search.shared.remove(r)
+    ):
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail=f'Search is not shared with {permission_id}',
+        )
+    search.permissions.remove(target)
     if search.is_changed:
         await search.save_changes()
     return SuccessPayloadOutput(payload=SearchOutput.from_obj(search))
