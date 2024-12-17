25a185293c3506e9e943db78ceed2dc51846fa56
alikhvn <theololo057@gmail.com>
2024-12-17 21:03:56 +0500

    front: add user creation and viewing functionality with form handling

---
+++ frontend/src/modules/users/view.tsx
@@ -0,0 +1,67 @@
+import { Breadcrumbs, Stack, Typography } from "@mui/material";
+import { getRouteApi } from "@tanstack/react-router";
+import { ErrorHandler, Link } from "components";
+import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
+import { userApi } from "store";
+import { UpdateUserT } from "types";
+import { toastApiError } from "utils";
+import { UserForm } from "./components/user_form";
+
+const routeApi = getRouteApi("/_authenticated/users/$userId");
+
+const UserView = () => {
+    const { t } = useTranslation();
+    const { userId } = routeApi.useParams();
+
+    const { data, error } = userApi.useGetUserQuery(userId);
+
+    const [updateUser, { isLoading: updateLoading }] =
+        userApi.useUpdateUserMutation();
+
+    const onSubmit = (formData: UpdateUserT) => {
+        const user = data?.payload;
+        if (!user) return;
+
+        updateUser({
+            id: user.id,
+            ...formData,
+        })
+            .unwrap()
+            .then(() => {
+                toast.success(t("users.update.success"));
+            })
+            .catch(toastApiError);
+    };
+
+    if (error) {
+        return <ErrorHandler error={error} message="users.item.fetch.error" />;
+    }
+
+    if (!data) return null;
+
+    const user = data.payload;
+
+    return (
+        <Stack px={4} pb={4} gap={2}>
+            <Breadcrumbs sx={{ mb: 2 }}>
+                <Link to="/users" underline="hover">
+                    <Typography fontSize={24} fontWeight="bold">
+                        {t("users.title")}
+                    </Typography>
+                </Link>
+                <Typography fontSize={24} fontWeight="bold">
+                    {user.name}
+                </Typography>
+            </Breadcrumbs>
+
+            <UserForm
+                defaultValues={user}
+                onSubmit={onSubmit}
+                loading={updateLoading}
+            />
+        </Stack>
+    );
+};
+
+export { UserView };
