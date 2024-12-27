ee06245616bee633be28af30df15d5de0adca9e4
alikhvn <theololo057@gmail.com>
2024-12-27 20:21:13 +0500

    totp deletion

--- frontend/src/modules/profile/components/account_security.tsx
+++ frontend/src/modules/profile/components/account_security.tsx
@@ -1,269 +1,21 @@
-import CloseIcon from "@mui/icons-material/Close";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import KeyIcon from "@mui/icons-material/Key";
+import LockIcon from "@mui/icons-material/Lock";
 import { LoadingButton } from "@mui/lab";
-import {
-    Box,
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    IconButton,
-    Stack,
-    TextField,
-    Typography,
-} from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import utc from "dayjs/plugin/utc";
-import { QRCodeSVG } from "qrcode.react";
-import { FC, useState } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { toast } from "react-toastify";
 import { userApi } from "store";
 import { TOTPDataT } from "types";
 import { toastApiError } from "utils";
+import { DeleteTOTPDialog } from "./dialogs/delete_totp_dialog";
+import { TFASetupDialog } from "./dialogs/tfa_setup_dialog";
+import { ToggleTFADialog } from "./dialogs/toggle_tfa_dialog";
 
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
-
-interface ITFASetupDialogProps {
-    open: boolean;
-    data: TOTPDataT | null;
-    onClose: () => void;
-}
-
-const TFASetupDialog: FC<ITFASetupDialogProps> = ({ open, data, onClose }) => {
-    const { t } = useTranslation();
-
-    const [code, setCode] = useState("");
-
-    const [updateMFASettings, { isLoading }] =
-        userApi.useUpdateMFASettingsMutation();
-
-    const handleClose = () => {
-        setCode("");
-        onClose();
-    };
-
-    const handleClickCopySecret = () => {
-        if (!data) return;
-
-        navigator.clipboard.writeText(data.secret).then(() => {
-            toast.success(t("clipboard.copied"));
-        });
-    };
-
-    const handleClickConfirm = () => {
-        if (!data) return;
-        if (!code) return;
-
-        updateMFASettings({
-            is_enabled: false,
-            mfa_totp_code: code,
-        })
-            .unwrap()
-            .then(() => {
-                toast.success(t("tfa.setup.success"));
-                handleClose();
-            })
-            .catch(toastApiError);
-    };
-
-    if (!data) return null;
-
-    return (
-        <Dialog
-            open={open}
-            onClose={(_, reason) => {
-                if (reason === "backdropClick" || reason === "escapeKeyDown") {
-                    return;
-                }
-                handleClose();
-            }}
-            fullWidth
-        >
-            <DialogTitle
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                gap={1}
-            >
-                <Typography fontWeight="bold">
-                    {t("tfa.setup.title")}
-                </Typography>
-
-                <IconButton onClick={onClose} size="small" disabled={isLoading}>
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-
-            <DialogContent>
-                <Stack spacing={2} alignItems="center">
-                    <Typography>{t("tfa.setup.description")}</Typography>
-
-                    <QRCodeSVG value={data.link} size={200} marginSize={3} />
-
-                    <Stack
-                        border={1}
-                        borderColor="divider"
-                        borderRadius={2}
-                        p={1}
-                        spacing={1}
-                        alignItems="center"
-                        bgcolor="info.dark"
-                    >
-                        <Typography align="center">
-                            <Typography component="span" fontWeight="bold">
-                                {t("tfa.setup.scanNotWorking")}
-                            </Typography>{" "}
-                            {t("tfa.setup.copySecret")}
-                        </Typography>
-
-                        <Box display="flex" alignItems="center" gap={1}>
-                            <Typography fontWeight="bold">
-                                {data.secret}
-                            </Typography>
-
-                            <IconButton
-                                onClick={handleClickCopySecret}
-                                size="small"
-                            >
-                                <ContentCopyIcon />
-                            </IconButton>
-                        </Box>
-                    </Stack>
-
-                    <Typography>{t("tfa.setup.secret")}</Typography>
-
-                    <TextField
-                        placeholder={t("tfa.enterCode")}
-                        value={code}
-                        onChange={(e) => setCode(e.target.value)}
-                        size="small"
-                    />
-                </Stack>
-            </DialogContent>
-
-            <DialogActions>
-                <Button
-                    onClick={handleClose}
-                    variant="outlined"
-                    color="error"
-                    disabled={isLoading}
-                >
-                    {t("cancel")}
-                </Button>
-
-                <LoadingButton
-                    onClick={handleClickConfirm}
-                    variant="outlined"
-                    disabled={!code}
-                    loading={isLoading}
-                >
-                    {t("tfa.setup.confirm")}
-                </LoadingButton>
-            </DialogActions>
-        </Dialog>
-    );
-};
-
-interface IToggleTFADialogProps {
-    open: boolean;
-    enabled: boolean;
-    onClose: () => void;
-}
-
-const ToggleTFADialog: FC<IToggleTFADialogProps> = ({
-    open,
-    enabled,
-    onClose,
-}) => {
-    const { t } = useTranslation();
-
-    const [code, setCode] = useState("");
-
-    const [updateMFASettings, { isLoading }] =
-        userApi.useUpdateMFASettingsMutation();
-
-    const handleClose = () => {
-        setCode("");
-        onClose();
-    };
-
-    const handleClickConfirm = () => {
-        updateMFASettings({
-            is_enabled: !enabled,
-            mfa_totp_code: code,
-        })
-            .unwrap()
-            .then(() => {
-                toast.success(
-                    t(enabled ? "tfa.disable.success" : "tfa.enable.success"),
-                );
-                handleClose();
-            })
-            .catch(toastApiError);
-    };
-
-    return (
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                gap={1}
-            >
-                <Typography fontWeight="bold">
-                    {t(enabled ? "tfa.disable.title" : "tfa.enable.title")}
-                </Typography>
-
-                <IconButton onClick={onClose} size="small" disabled={isLoading}>
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-
-            <DialogContent>
-                <Stack spacing={2} alignItems="center">
-                    <Typography>
-                        {t(
-                            enabled
-                                ? "tfa.disable.confirmation"
-                                : "tfa.enable.confirmation",
-                        )}
-                    </Typography>
-
-                    <TextField
-                        placeholder={t("tfa.enterCode")}
-                        value={code}
-                        onChange={(e) => setCode(e.target.value)}
-                        size="small"
-                    />
-                </Stack>
-            </DialogContent>
-
-            <DialogActions>
-                <Button
-                    onClick={handleClose}
-                    variant="outlined"
-                    color="error"
-                    disabled={isLoading}
-                >
-                    {t("cancel")}
-                </Button>
-
-                <LoadingButton
-                    onClick={handleClickConfirm}
-                    variant="outlined"
-                    disabled={!code}
-                    loading={isLoading}
-                >
-                    {t("tfa.setup.confirm")}
-                </LoadingButton>
-            </DialogActions>
-        </Dialog>
-    );
-};
 
 export const AccountSecurity = () => {
     const { t } = useTranslation();
@@ -273,11 +25,9 @@ export const AccountSecurity = () => {
     const [createOTP, { isLoading: createOTPLoading }] =
         userApi.useCreateOTPMutation();
 
-    const [deleteOTP, { isLoading: deleteOTPLoading }] =
-        userApi.useDeleteOTPMutation();
-
     const [TOTPData, setTOTPData] = useState<TOTPDataT | null>(null);
     const [toggleTFADialogOpen, setToggleTFADialogOpen] = useState(false);
+    const [deleteTOTPDialogOpen, setDeleteTOTPDialogOpen] = useState(false);
 
     const tfaEnabled = mfaSettings?.payload.is_enabled ?? false;
     const totpCreated = mfaSettings?.payload.totp.created_at ?? null;
@@ -292,12 +42,7 @@ export const AccountSecurity = () => {
     };
 
     const handleClickDelete = () => {
-        deleteOTP({ mfa_totp_code: null })
-            .unwrap()
-            .then(() => {
-                toast.success(t("tfa.totp.deleted"));
-            })
-            .catch(toastApiError);
+        setDeleteTOTPDialogOpen(true);
     };
 
     const handleToggleTFA = () => {
@@ -334,10 +79,10 @@ export const AccountSecurity = () => {
                         color={totpCreated ? "success" : "error"}
                     >
                         {totpCreated
-                            ? dayjs
+                            ? `${t("tfa.totp.status.set")} ${dayjs
                                   .utc(totpCreated)
                                   .local()
-                                  .format("DD MMM YYYY HH:mm")
+                                  .format("DD MMM YYYY HH:mm")}`
                             : t("tfa.totp.status.notSet")}
                     </Typography>
                 </Typography>
@@ -355,39 +100,39 @@ export const AccountSecurity = () => {
                 onClose={() => setToggleTFADialogOpen(false)}
             />
 
+            <DeleteTOTPDialog
+                open={deleteTOTPDialogOpen}
+                tfaEnabled={tfaEnabled}
+                onClose={() => setDeleteTOTPDialogOpen(false)}
+            />
+
             <Box display="flex" gap={1}>
-                {!tfaEnabled && !totpCreated && (
-                    <LoadingButton
-                        onClick={handleClickSetup}
-                        variant="outlined"
-                        size="small"
-                        color="secondary"
-                        loading={isLoading || deleteOTPLoading}
-                    >
-                        {t("tfa.setup")}
-                    </LoadingButton>
-                )}
-
-                {!tfaEnabled && totpCreated && (
-                    <LoadingButton
-                        onClick={handleClickDelete}
-                        variant="outlined"
-                        size="small"
-                        color="secondary"
-                        loading={isLoading || createOTPLoading}
-                    >
-                        {t("tfa.totp.delete")}
-                    </LoadingButton>
-                )}
-
-                <Button
-                    onClick={handleToggleTFA}
+                <LoadingButton
+                    onClick={totpCreated ? handleClickDelete : handleClickSetup}
                     variant="outlined"
                     size="small"
-                    color={tfaEnabled ? "error" : "primary"}
+                    color={totpCreated ? "error" : "primary"}
+                    loading={isLoading || createOTPLoading}
+                    startIcon={<KeyIcon />}
                 >
-                    {tfaEnabled ? t("tfa.disable") : t("tfa.enable")}
-                </Button>
+                    {t(
+                        totpCreated
+                            ? "tfa.totp.delete.title"
+                            : "tfa.setup.title",
+                    )}
+                </LoadingButton>
+
+                {totpCreated && (
+                    <Button
+                        onClick={handleToggleTFA}
+                        variant="outlined"
+                        size="small"
+                        color={tfaEnabled ? "error" : "secondary"}
+                        startIcon={<LockIcon />}
+                    >
+                        {tfaEnabled ? t("tfa.disable") : t("tfa.enable")}
+                    </Button>
+                )}
             </Box>
         </Stack>
     );
---
+++ frontend/src/modules/profile/components/dialogs/delete_totp_dialog.tsx
@@ -0,0 +1,112 @@
+import { LoadingButton } from "@mui/lab";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    IconButton,
+    Stack,
+    TextField,
+    Typography,
+} from "@mui/material";
+
+import CloseIcon from "@mui/icons-material/Close";
+import { FC, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
+import { userApi } from "store";
+import { toastApiError } from "utils";
+
+interface IDeleteTOTPDialogProps {
+    open: boolean;
+    tfaEnabled: boolean;
+    onClose: () => void;
+}
+
+export const DeleteTOTPDialog: FC<IDeleteTOTPDialogProps> = ({
+    open,
+    tfaEnabled,
+    onClose,
+}) => {
+    const { t } = useTranslation();
+
+    const [deleteOTP, { isLoading }] = userApi.useDeleteOTPMutation();
+
+    const [code, setCode] = useState<string | null>(null);
+
+    const handleClose = () => {
+        setCode(null);
+        onClose();
+    };
+
+    const handleClickConfirm = () => {
+        deleteOTP({ mfa_totp_code: code })
+            .unwrap()
+            .then(() => {
+                toast.success(t("tfa.totp.delete.success"));
+                handleClose();
+            })
+            .catch(toastApiError);
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose} maxWidth="xs">
+            <DialogTitle
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                gap={1}
+            >
+                <Typography fontWeight="bold">
+                    {t("tfa.totp.delete.title")}
+                </Typography>
+
+                <IconButton onClick={onClose} size="small" disabled={isLoading}>
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+
+            <DialogContent>
+                <Stack spacing={2} alignItems="center">
+                    <Typography>
+                        {t(
+                            tfaEnabled
+                                ? "tfa.totp.delete.confirmationWithCode"
+                                : "tfa.totp.delete.confirmation",
+                        )}
+                    </Typography>
+
+                    {tfaEnabled && (
+                        <TextField
+                            placeholder={t("tfa.enterCode")}
+                            value={code}
+                            onChange={(e) => setCode(e.target.value)}
+                            size="small"
+                        />
+                    )}
+                </Stack>
+            </DialogContent>
+
+            <DialogActions>
+                <Button
+                    onClick={handleClose}
+                    variant="outlined"
+                    color="error"
+                    disabled={isLoading}
+                >
+                    {t("cancel")}
+                </Button>
+
+                <LoadingButton
+                    onClick={handleClickConfirm}
+                    variant="outlined"
+                    disabled={!code}
+                    loading={isLoading}
+                >
+                    {t("tfa.confirm")}
+                </LoadingButton>
+            </DialogActions>
+        </Dialog>
+    );
+};
