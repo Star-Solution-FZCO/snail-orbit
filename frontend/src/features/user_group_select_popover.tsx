89443b91236dc1c20b3d05c031a3fd6a213c2af3
Lenarin <Vladislavakia425@gmail.com>
2025-02-20 00:56:24 +0300

    front: Agile board added favorite functionality

---
+++ frontend/src/features/user_group_select_popover.tsx
@@ -0,0 +1,9 @@
+import { memo } from "react";
+
+type UserGroupSelectPopoverProps = {};
+
+export const UserGroupSelectPopover = memo(
+    (props: UserGroupSelectPopoverProps) => {},
+);
+
+UserGroupSelectPopover.displayName = "UserGroupSelectPopover";
---
+++ frontend/src/modules/agile_boards/components/agile_board_form/tabs/access.tsx
@@ -0,0 +1,6 @@
+import type {FC} from "react";
+import {Stack} from "@mui/material";
+
+export const Access: FC = () => {
+    return <Stack direction='column' gap={2}></Stack>
+}
