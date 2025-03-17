bd06ea1efbd2f813e3c68faf885902216ddd410b
BelCo <12194066-BelCo94@users.noreply.gitlab.com>
2025-03-18 00:21:20 +0100

    Add support for managing user encryption keys
    Introduced an `EncryptionKey` model and API routes for listing and creating encryption keys. Updated user model to include encryption keys and linked the new router in the settings module.

--- app/pm/api/routes/api/v1/settings/__init__.py
+++ app/pm/api/routes/api/v1/settings/__init__.py
@@ -4,6 +4,7 @@ from pm.api.context import current_user_context_dependency
 from pm.api.utils.router import APIRouter
 
 from .api_token import router as api_token_router
+from .encryption_key import router as encryption_key_router
 from .mfa import router as mfa_router
 from .ui_settings import router as ui_settings_router
 
@@ -15,5 +16,6 @@ router = APIRouter(
     dependencies=[Depends(current_user_context_dependency)],
 )
 router.include_router(api_token_router)
+router.include_router(encryption_key_router)
 router.include_router(mfa_router)
 router.include_router(ui_settings_router)
