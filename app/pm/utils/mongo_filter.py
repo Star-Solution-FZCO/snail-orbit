1b375befa3b07f0e8e2e2dfec2edc682a0741175
BelCo <nya.belco@gmail.com>
2025-03-21 00:00:43 +0100

    Add MongoDB filter and sort parsing utility
    Implemented `parse_filter` and `parse_sort` functions for constructing MongoDB queries from string inputs. Includes comprehensive unit tests to validate query parsing, logical operators, and sorting functionality.

---
+++ app/pm/utils/mongo_filter.py
@@ -0,0 +1,282 @@
+from typing import TYPE_CHECKING, Any, Mapping, Sequence, get_args
+
+import beanie.operators as bo
+import pyparsing as pp
+from beanie import Document
+
+if TYPE_CHECKING:
+    from beanie.odm.operators.find.array import BaseFindOperator
+    from pydantic.fields import FieldInfo
+
+
+__all__ = (
+    'parse_filter',
+    'parse_sort',
+)
+
+
+AVAILABLE_OPERATORS = [
+    'eq',
+    'ne',
+    'in',
+    'nin',
+    'lt',
+    'lte',
+    'gt',
+    'gte',
+    'contains',
+    'icontains',
+    'startswith',
+    'endswith',
+]
+AVAILABLE_OPERATORS_STRING = ' '.join(AVAILABLE_OPERATORS)
+OPERATOR_TO_TYPE = {
+    'list': {
+        'in': bo.In,
+        'nin': bo.NotIn,
+        'default': bo.Eq,
+    },
+    'int': {
+        'eq': bo.Eq,
+        'ne': bo.NE,
+        'lt': bo.LT,
+        'lte': bo.LTE,
+        'gt': bo.GT,
+        'gte': bo.GTE,
+        'in': bo.In,
+        'nin': bo.NotIn,
+        'default': bo.Eq,
+    },
+    'float': {
+        'eq': bo.Eq,
+        'ne': bo.NE,
+        'lt': bo.LT,
+        'lte': bo.LTE,
+        'gt': bo.GT,
+        'gte': bo.GTE,
+        'in': bo.In,
+        'nin': bo.NotIn,
+        'default': bo.Eq,
+    },
+    'str': {
+        'contains': lambda f, s: bo.RegEx(f, rf'.*{s}.*'),
+        'icontains': lambda f, s: bo.RegEx(f, rf'.*{s}.*', 'i'),
+        'startswith': lambda f, s: bo.RegEx(f, rf'^{s}.*'),
+        'endswith': lambda f, s: bo.RegEx(f, rf'.*{s}$'),
+        'ne': bo.NE,
+        'eq': bo.Eq,
+        'in': bo.In,
+        'nin': bo.NotIn,
+        'default': bo.Eq,
+    },
+}
+
+
+class BinOp:
+    operands: list
+
+    def __init__(self, tokens: pp.ParseResults) -> None:
+        self.operands = tokens[0][::2]
+
+
+class AndOp(BinOp):
+    pass
+
+
+class OrOp(BinOp):
+    pass
+
+
+class NotOp:
+    operand: Any
+
+    def __init__(self, tokens: pp.ParseResults) -> None:
+        self.operand = tokens[0][1]
+
+
+FIELD_PART = pp.Group(
+    pp.Word(pp.alphanums) + pp.ZeroOrMore(pp.Char('_') + pp.Word(pp.alphanums))
+).setResultsName('field_part')
+
+FIELD_NAME = FIELD_PART + pp.Optional(pp.OneOrMore(pp.Suppress('__') + FIELD_PART))
+
+
+FILTER_EXPRESSION = pp.Group(
+    FIELD_NAME
+    + pp.Optional(
+        pp.Group(
+            pp.Suppress('___') + pp.oneOf(AVAILABLE_OPERATORS_STRING)
+        ).setResultsName('operator')
+    )
+    + pp.Suppress(':')
+    + pp.Group(
+        pp.DelimitedList(pp.QuotedString('"') | pp.Word(pp.alphanums + '.'))
+    ).setResultsName('value')
+)
+
+
+FILTER_STRING = pp.infixNotation(
+    FILTER_EXPRESSION,
+    [
+        (pp.Keyword('not', caseless=True), 1, pp.opAssoc.RIGHT, NotOp),
+        (pp.Keyword('and', caseless=True), 2, pp.opAssoc.LEFT, AndOp),
+        (pp.Keyword('or', caseless=True), 2, pp.opAssoc.LEFT, OrOp),
+    ],
+)
+
+
+SORT_EXPRESSION = pp.Group(FIELD_NAME).setResultsName('asc_field') | pp.Group(
+    pp.Suppress('-') + FIELD_NAME
+).setResultsName('desc_field')
+
+SORT_STRING = pp.DelimitedList(SORT_EXPRESSION)
+
+
+def convert_value(value: Any, inner_type: type) -> Any:
+    if isinstance(value, list):
+        return [convert_value(v, inner_type) for v in value]
+    if inner_type == bool:
+        return str(value).lower() not in ('false', '0')
+    try:
+        return inner_type(value)
+    except TypeError:
+        return value
+
+
+def get_operator(
+    field_name: str, field_type: 'FieldInfo', value: Any, operator: str = None
+) -> 'BaseFindOperator':
+    outer = field_type.annotation
+    inner = args_[0] if (args_ := get_args(outer)) else outer
+    cls_name = outer.__name__
+    value = convert_value(value, inner)
+    if operator is None:
+        if str.lower(cls_name) not in OPERATOR_TO_TYPE:
+            return bo.Eq(field_name, value)
+        operator = 'default'
+    if str.lower(cls_name) not in OPERATOR_TO_TYPE:
+        raise ValueError(f'unknown class name {cls_name}')
+    try:
+        op_cls = OPERATOR_TO_TYPE[str.lower(cls_name)][operator]
+    except KeyError as err:
+        raise ValueError(f'invalid operator {operator} for {field_name}') from err
+    return op_cls(field_name, value)
+
+
+def get_available_field(
+    field_parts: list[str],
+    model: type[Document],
+    available_fields: Sequence[str] | None = None,
+) -> tuple[str, Any]:
+    if not field_parts:
+        raise ValueError('empty field')
+    field_name = '.'.join(field_parts)
+    field_name_dash = '__'.join(field_parts)
+    if (
+        available_fields
+        and field_name not in available_fields
+        and field_name_dash not in available_fields
+    ):
+        raise PermissionError(f'you can not search by field {field_name}')
+    try:
+        field = model.model_fields[field_parts[0]]
+    except KeyError as err:
+        raise PermissionError(
+            f'you can not search by this field "{field_name}"'
+        ) from err
+    return field_name, field
+
+
+def transform_filter_expression(
+    part: pp.ParseResults,
+    model: type[Document],
+    available_fields: Sequence[str] | None = None,
+) -> 'BaseFindOperator':
+    field_parts: list[str] = [''.join(part[0])]
+    value = None
+    operator = None
+    for p in part[1:]:
+        if p.get_name() == 'field_part':
+            field_parts += [''.join(p)]
+        if p.get_name() == 'operator':
+            operator = ''.join(p)
+        if p.get_name() == 'value':
+            if len(p) > 1:
+                value = list(p)
+            else:
+                value = ''.join(p)
+    field_name, field = get_available_field(field_parts, model, available_fields)
+    return get_operator(field_name, field, value, operator)
+
+
+def transform_filter(
+    parsed: pp.ParseResults,
+    model: type[Document],
+    available_fields: Sequence[str] = None,
+    not_: bool = False,
+) -> Mapping[str, Any]:
+    if isinstance(parsed, OrOp):
+        op_ = bo.Nor if not_ else bo.Or
+        return op_(
+            *(
+                transform_filter(part, model, available_fields)
+                for part in parsed.operands
+            )
+        )
+    if isinstance(parsed, AndOp):
+        if not_:
+            return bo.Or(
+                *(
+                    transform_filter(part, model, available_fields, not_=True)
+                    for part in parsed.operands
+                )
+            )
+        return bo.And(
+            *(
+                transform_filter(part, model, available_fields)
+                for part in parsed.operands
+            )
+        )
+    if isinstance(parsed, NotOp):
+        return transform_filter(parsed.operand, model, available_fields, not_=not not_)
+    parsed_expression = transform_filter_expression(parsed, model, available_fields)
+    return bo.Not(parsed_expression) if not_ else parsed_expression
+
+
+def transform_sort_expression(
+    part: pp.ParseResults,
+    model: type[Document],
+    available_fields: Sequence[str] | None = None,
+) -> str:
+    direction = '-' if part.get_name() == 'desc_field' else ''
+    field_parts: list[str] = [''.join(p) for p in part]
+    field_name, _ = get_available_field(field_parts, model, available_fields)
+    return direction + field_name
+
+
+def transform_sort(
+    parsed: pp.ParseResults,
+    model: type[Document],
+    available_fields: Sequence[str] | None = None,
+) -> list[str]:
+    return [transform_sort_expression(part, model, available_fields) for part in parsed]
+
+
+def parse_filter(
+    model: type[Document], query: str, available_fields: Sequence[str] | None = None
+) -> Mapping[str, Any]:
+    if not query:
+        return {}
+    return transform_filter(
+        FILTER_STRING.parse_string(query, False)[0], model, available_fields
+    )
+
+
+def parse_sort(
+    model: type[Document], query: str, available_fields: Sequence[str] | None = None
+) -> list[str]:
+    if not query:
+        return []
+    return transform_sort(
+        SORT_STRING.parse_string(query, False), model, available_fields
+    )
---
+++ app/tests/mongo_filter/test_mongo_filter.py
@@ -0,0 +1,349 @@
+import pytest
+from beanie import Document
+from pydantic import BaseModel
+
+
+class ObjectField(BaseModel):
+    name: str
+
+
+class MockDocument(Document):
+    name: str
+    str_field: str
+    int_field: int
+    int_list_field: list[int]
+    str_list_field: list[str]
+    bool_field: bool
+    float_field: float
+    float_list_field: list[float]
+    obj: ObjectField
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    'filter_, expected',
+    [
+        pytest.param('', {}, id='empty'),
+        pytest.param('name: test', {'name': 'test'}, id='name'),
+        pytest.param('int_field:10', {'int_field': 10}, id='int_field'),
+        pytest.param('int_field___eq:10', {'int_field': 10}, id='int_field___eq'),
+        pytest.param(
+            'int_field___ne:10', {'int_field': {'$ne': 10}}, id='int_field___ne'
+        ),
+        pytest.param(
+            'int_field___lt:10', {'int_field': {'$lt': 10}}, id='int_field___lt'
+        ),
+        pytest.param(
+            'int_field___lte:10', {'int_field': {'$lte': 10}}, id='int_field___lte'
+        ),
+        pytest.param(
+            'int_field___gt:10', {'int_field': {'$gt': 10}}, id='int_field___gt'
+        ),
+        pytest.param(
+            'int_field___gte:10', {'int_field': {'$gte': 10}}, id='int_field___gte'
+        ),
+        pytest.param(
+            'int_field___in:10,20',
+            {'int_field': {'$in': [10, 20]}},
+            id='int_field___in',
+        ),
+        pytest.param(
+            'int_field___nin:10,20',
+            {'int_field': {'$nin': [10, 20]}},
+            id='int_field___nin',
+        ),
+        pytest.param('str_field:"test"', {'str_field': 'test'}, id='str_field___quote'),
+        pytest.param(
+            'str_field:test', {'str_field': 'test'}, id='str_field___no_quote'
+        ),
+        pytest.param(
+            'str_field___eq:"test test"',
+            {'str_field': 'test test'},
+            id='str_field___eq',
+        ),
+        pytest.param(
+            'str_field___ne:"test"', {'str_field': {'$ne': 'test'}}, id='str_field___ne'
+        ),
+        pytest.param(
+            'str_field___contains:"test"',
+            {'str_field': {'$regex': '.*test.*'}},
+            id='str_field___contains',
+        ),
+        pytest.param(
+            'str_field___icontains:"test"',
+            {'str_field': {'$regex': '.*test.*', '$options': 'i'}},
+            id='str_field___icontains',
+        ),
+        pytest.param(
+            'str_field___startswith:"test"',
+            {'str_field': {'$regex': '^test.*'}},
+            id='str_field___startswith',
+        ),
+        pytest.param(
+            'str_field___endswith:"test"',
+            {'str_field': {'$regex': '.*test$'}},
+            id='str_field___endswith',
+        ),
+        pytest.param(
+            'str_field___in:"test1","test2"',
+            {'str_field': {'$in': ['test1', 'test2']}},
+            id='str_field___in',
+        ),
+        pytest.param(
+            'str_field___nin:"test1","test2"',
+            {'str_field': {'$nin': ['test1', 'test2']}},
+            id='str_field___nin',
+        ),
+        pytest.param('bool_field:True', {'bool_field': True}, id='bool_field___True'),
+        pytest.param(
+            'bool_field:False', {'bool_field': False}, id='bool_field___False'
+        ),
+        pytest.param('bool_field:1', {'bool_field': True}, id='bool_field___1'),
+        pytest.param('bool_field:0', {'bool_field': False}, id='bool_field___0'),
+        pytest.param('float_field:1.0', {'float_field': 1.0}, id='float_field___1.0'),
+        pytest.param('float_field:0', {'float_field': 0.0}, id='float_field___0'),
+        pytest.param('float_field:122', {'float_field': 122.0}, id='float_field___0'),
+        pytest.param('float_field:1.2', {'float_field': 1.2}, id='float_field___1.2'),
+        pytest.param(
+            'float_field___eq:1.2', {'float_field': 1.2}, id='float_field___eq'
+        ),
+        pytest.param(
+            'float_field___ne:1.2', {'float_field': {'$ne': 1.2}}, id='float_field___ne'
+        ),
+        pytest.param(
+            'float_field___lt:1.2', {'float_field': {'$lt': 1.2}}, id='float_field___lt'
+        ),
+        pytest.param(
+            'float_field___lte:1.2',
+            {'float_field': {'$lte': 1.2}},
+            id='float_field___lte',
+        ),
+        pytest.param(
+            'float_field___gt:1.2', {'float_field': {'$gt': 1.2}}, id='float_field___gt'
+        ),
+        pytest.param(
+            'float_field___gte:1.2',
+            {'float_field': {'$gte': 1.2}},
+            id='float_field___gte',
+        ),
+        pytest.param(
+            'float_field___in:1.2,1.3',
+            {'float_field': {'$in': [1.2, 1.3]}},
+            id='float_field___in',
+        ),
+        pytest.param(
+            'float_field___nin:1.2,1.3,30',
+            {'float_field': {'$nin': [1.2, 1.3, 30.0]}},
+            id='float_field___nin',
+        ),
+        pytest.param('int_list_field:1', {'int_list_field': 1}, id='int_list_field'),
+        pytest.param(
+            'int_list_field___in:1,2,3',
+            {'int_list_field': {'$in': [1, 2, 3]}},
+            id='int_list_field___in',
+        ),
+        pytest.param(
+            'int_list_field___nin:1,2,3',
+            {'int_list_field': {'$nin': [1, 2, 3]}},
+            id='int_list_field___nin',
+        ),
+        pytest.param(
+            'str_list_field:"1"', {'str_list_field': '1'}, id='str_list_field'
+        ),
+        pytest.param(
+            'str_list_field___in:"1","2","3"',
+            {'str_list_field': {'$in': ['1', '2', '3']}},
+            id='str_list_field___in',
+        ),
+        pytest.param(
+            'str_list_field___nin:"xxx","2",yyyy',
+            {'str_list_field': {'$nin': ['xxx', '2', 'yyyy']}},
+            id='str_list_field___nin',
+        ),
+        pytest.param(
+            'float_list_field:1.0', {'float_list_field': 1.0}, id='float_list_field'
+        ),
+        pytest.param(
+            'float_list_field___in:1.0,2.0,3.0',
+            {'float_list_field': {'$in': [1.0, 2.0, 3.0]}},
+            id='float_list_field___in',
+        ),
+        pytest.param(
+            'float_list_field___nin:1.0,2.0,3.0',
+            {'float_list_field': {'$nin': [1.0, 2.0, 3.0]}},
+            id='float_list_field___nin',
+        ),
+        pytest.param('obj__name:test', {'obj.name': 'test'}, id='obj.name'),
+        pytest.param(
+            'str_field: test and int_field: 10',
+            {'$and': [{'str_field': 'test'}, {'int_field': 10}]},
+            id='simple_and',
+        ),
+        pytest.param(
+            'str_field: test and int_field: 10 and bool_field: True',
+            {
+                '$and': [
+                    {'str_field': 'test'},
+                    {'int_field': 10},
+                    {'bool_field': True},
+                ],
+            },
+            id='and_and',
+        ),
+        pytest.param(
+            'str_field: test or int_field: 10',
+            {'$or': [{'str_field': 'test'}, {'int_field': 10}]},
+            id='simple_or',
+        ),
+        pytest.param(
+            'str_field: test and int_field: 10 or bool_field: True',
+            {
+                '$or': [
+                    {'$and': [{'str_field': 'test'}, {'int_field': 10}]},
+                    {'bool_field': True},
+                ],
+            },
+            id='and_or',
+        ),
+        pytest.param(
+            'str_field: test and (int_field: 10 or bool_field: True)',
+            {
+                '$and': [
+                    {'str_field': 'test'},
+                    {'$or': [{'int_field': 10}, {'bool_field': True}]},
+                ],
+            },
+            id='and_or_brackets',
+        ),
+        pytest.param(
+            'str_field: test or not int_field: 10',
+            {'$or': [{'str_field': 'test'}, {'int_field': {'$not': {'$eq': 10}}}]},
+            id='and_not',
+        ),
+        pytest.param(
+            'not str_field: test', {'str_field': {'$not': {'$eq': 'test'}}}, id='not'
+        ),
+        pytest.param(
+            'str_field: test and not int_field: 10',
+            {'$and': [{'str_field': 'test'}, {'int_field': {'$not': {'$eq': 10}}}]},
+            id='and_not',
+        ),
+        pytest.param(
+            'str_field: test and not (int_field: 10 or bool_field: True)',
+            {
+                '$and': [
+                    {'str_field': 'test'},
+                    {'$nor': [{'int_field': 10}, {'bool_field': True}]},
+                ]
+            },
+            id='and_not_brackets',
+        ),
+        pytest.param(
+            'not (int_field: 10 or bool_field: True)',
+            {'$nor': [{'int_field': 10}, {'bool_field': True}]},
+            id='nor',
+        ),
+        pytest.param(
+            'not (int_field: 10 or bool_field: True) and str_field: test',
+            {
+                '$and': [
+                    {'$nor': [{'int_field': 10}, {'bool_field': True}]},
+                    {'str_field': 'test'},
+                ]
+            },
+            id='nor_and',
+        ),
+        pytest.param(
+            'not (int_field: 10 or bool_field: True) or str_field: test',
+            {
+                '$or': [
+                    {'$nor': [{'int_field': 10}, {'bool_field': True}]},
+                    {'str_field': 'test'},
+                ]
+            },
+            id='nor_or',
+        ),
+        pytest.param(
+            'not (int_field: 10 or bool_field: True) and not str_field: test',
+            {
+                '$and': [
+                    {'$nor': [{'int_field': 10}, {'bool_field': True}]},
+                    {'str_field': {'$not': {'$eq': 'test'}}},
+                ]
+            },
+            id='nor_and_not',
+        ),
+        pytest.param(
+            'not (int_field: 10 or bool_field: True) or not str_field: test',
+            {
+                '$or': [
+                    {'$nor': [{'int_field': 10}, {'bool_field': True}]},
+                    {'str_field': {'$not': {'$eq': 'test'}}},
+                ]
+            },
+            id='nor_or_not',
+        ),
+        pytest.param(
+            'not (int_field: 10 or bool_field: True) and not (str_field: test or float_field: 1.0)',
+            {
+                '$and': [
+                    {'$nor': [{'int_field': 10}, {'bool_field': True}]},
+                    {'$nor': [{'str_field': 'test'}, {'float_field': 1.0}]},
+                ]
+            },
+            id='nor_and_nor',
+        ),
+        pytest.param(
+            'not (int_field: 10 or bool_field: True) or not (str_field: test or float_field: 1.0)',
+            {
+                '$or': [
+                    {'$nor': [{'int_field': 10}, {'bool_field': True}]},
+                    {'$nor': [{'str_field': 'test'}, {'float_field': 1.0}]},
+                ]
+            },
+            id='nor_or_nor',
+        ),
+        pytest.param(
+            'not (int_field: 10 or bool_field: True or str_field: test)',
+            {'$nor': [{'int_field': 10}, {'bool_field': True}, {'str_field': 'test'}]},
+            id='nor_or_nor',
+        ),
+        pytest.param(
+            'not (int_field: 10 and bool_field: True and str_field: test)',
+            {
+                '$or': [
+                    {'int_field': {'$not': {'$eq': 10}}},
+                    {'bool_field': {'$not': {'$eq': True}}},
+                    {'str_field': {'$not': {'$eq': 'test'}}},
+                ]
+            },
+            id='not_and',
+        ),
+        pytest.param('not not int_field: 10', {'int_field': 10}, id='not_not'),
+    ],
+)
+async def test_mongo_filter(filter_: str, expected: dict) -> None:
+    from pm.utils.mongo_filter import parse_filter
+
+    assert parse_filter(MockDocument, filter_) == expected
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    'sort_, expected',
+    [
+        pytest.param('name', ['name'], id='name'),
+        pytest.param('-name', ['-name'], id='-name'),
+        pytest.param('name,-int_field', ['name', '-int_field'], id='name,-int_field'),
+        pytest.param(
+            'name,-int_field,str_field',
+            ['name', '-int_field', 'str_field'],
+            id='name,-int_field,str_field',
+        ),
+        pytest.param('', [], id='empty'),
+    ],
+)
+async def test_mongo_sort(sort_: str, expected: list[str]) -> None:
+    from pm.utils.mongo_filter import parse_sort
+
+    assert parse_sort(MockDocument, sort_) == expected
