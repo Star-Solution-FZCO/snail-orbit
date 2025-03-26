d9f8f854de93aa6a0898d4852adc9754e1aab5df
Lenarin <Vladislavakia425@gmail.com>
2025-03-27 02:57:13 +0300

    Fixes

--- frontend/src/theme/contrast-text.tsx
+++ frontend/src/theme/contrast-text.tsx
@@ -1,12 +1,12 @@
 import { getContrastRatio } from "@mui/material";
 import { theme } from "./index";
 
-export const getContrastText = (color: string) => {
+export const getContrastText = (color: string): string => {
     try {
         return getContrastRatio(color, "#fff") > theme.palette.contrastThreshold
             ? "#fff"
             : "#111";
     } catch {
-        return null;
+        return getContrastText("#000");
     }
 };
