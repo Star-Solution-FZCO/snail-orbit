8e5bd3456fbd57d5384c1ceaaf8c6a51f846f69c
BelCo <nya.belco@gmail.com>
2025-04-30 21:12:18 +0200

    Add OpenAPI schema
    - Added openapi.json file to the repo
    - Added `gen-openapi-schema` pre-commit hook for managing OpenAPI schemas.
    - Implemented CLI command to validate (`check`) OpenAPI schemas using `deepdiff` (if installed)
    - Updated `.gitignore` and `poetry.lock` to accommodate these changes.
    
    Change-Id: Ic3952a44d17cc23033930a5b4724d1f3c80fca25

--- app/pm/cli/api.py
+++ app/pm/cli/api.py
@@ -1,5 +1,9 @@
 # pylint: disable=import-outside-toplevel
 import argparse
+import json
+import os
+from collections.abc import Generator
+from contextlib import contextmanager
 
 __all__ = ('add_api_args',)
 
@@ -15,26 +19,67 @@ def run_api_server(args: argparse.Namespace) -> None:
     )
 
 
-def gen_openapi(args: argparse.Namespace) -> None:
-    import json
-    import os
+@contextmanager
+def set_envs(envs: dict[str, str]) -> Generator[None]:
+    old_envs = {}
+    for k, v in envs.items():
+        old_envs[k] = os.getenv(k)
+        os.environ[k] = v
+    try:
+        yield
+    finally:
+        for k, v in old_envs.items():
+            if v is None:
+                os.unsetenv(k)
+                continue
+            os.environ[k] = v
 
+
+def gen_openapi_schema() -> dict:
     from fastapi.openapi.utils import get_openapi
 
-    # this secret is used for generating the openapi schema only
-    os.environ['SNAIL_ORBIT_JWT_SECRET'] = 'nosec'  # nosec: hardcoded_password_string
+    envs = {
+        # this secret is used for generating the openapi schema only
+        'SNAIL_ORBIT_JWT_SECRET': 'nosec',  # nosec: hardcoded_password_string
+    }
+    with set_envs(envs):
+        from pm.api.app import app
 
-    from pm.api.app import app
+        return get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            routes=app.routes,
+        )
 
-    openapi_schema = get_openapi(
-        title=app.title,
-        version=app.version,
-        openapi_version=app.openapi_version,
-        description=app.description,
-        routes=app.routes,
-    )
+
+def gen_openapi(args: argparse.Namespace) -> None:
+    openapi_schema = gen_openapi_schema()
     with open(args.output, 'w', encoding='utf-8') as fp:
         json.dump(openapi_schema, fp=fp, indent=args.indent)
+        fp.write('\n')
+
+
+def check_openapi(args: argparse.Namespace) -> None:
+    try:
+        import deepdiff
+    except ImportError:
+        deepdiff = None
+
+    with open(args.file, 'r', encoding='utf-8') as in_file:
+        data = json.load(in_file)
+    openapi_schema = gen_openapi_schema()
+
+    if not deepdiff:
+        if data == openapi_schema:
+            return
+        raise ValueError('OpenAPI schema mismatch')
+
+    diff = deepdiff.DeepDiff(openapi_schema, data)
+    if not diff:
+        return
+    raise ValueError(f'OpenAPI schema mismatch:\n{diff.pretty()}')
 
 
 def add_api_args(parser: argparse.ArgumentParser) -> None:
@@ -47,6 +92,13 @@ def add_api_args(parser: argparse.ArgumentParser) -> None:
     server_parser.set_defaults(func=run_api_server)
 
     openapi_parser = subparsers.add_parser('openapi')
-    openapi_parser.add_argument('--indent', type=int, default=2)
-    openapi_parser.add_argument('-o', '--output', type=str, default='openapi.json')
-    openapi_parser.set_defaults(func=gen_openapi)
+    openapi_subparsers = openapi_parser.add_subparsers(required=True)
+
+    openapi_gen_parser = openapi_subparsers.add_parser('gen')
+    openapi_gen_parser.add_argument('--indent', type=int, default=2)
+    openapi_gen_parser.add_argument('-o', '--output', type=str, default='openapi.json')
+    openapi_gen_parser.set_defaults(func=gen_openapi)
+
+    openapi_check_parser = openapi_subparsers.add_parser('check')
+    openapi_check_parser.add_argument('-f', '--file', type=str, default='openapi.json')
+    openapi_check_parser.set_defaults(func=check_openapi)
