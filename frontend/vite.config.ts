7360cd45f5079b061e2ab937026c2047398ea077
5kif4a <theololo057@gmail.com>
2025-07-01 00:34:52 +0500

    refactor: simplify Vite config by removing loadEnv and using process.env directly
    Change-Id: I48cf718bd0a61eb3c4c9abcb264acbf02851d103

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,25 +3,18 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
-import {
-    defineConfig,
-    loadEnv,
-    splitVendorChunkPlugin,
-    type PluginOption,
-} from "vite";
+import { defineConfig, splitVendorChunkPlugin, type PluginOption } from "vite";
 import unusedCode from "vite-plugin-unused-code";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const oneYear = 60 * 60 * 24 * 365;
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
-
-    const sentryOrg = env.SENTRY_ORG || "";
-    const sentryProject = env.SENTRY_PROJECT || "";
-    const sentryRelease = env.SENTRY_RELEASE || "";
-    const sentryAuthToken = env.SENTRY_AUTH_TOKEN || "";
+export default defineConfig(() => {
+    const sentryOrg = process.env.SENTRY_ORG || "";
+    const sentryProject = process.env.SENTRY_PROJECT || "";
+    const sentryRelease = process.env.SENTRY_RELEASE || "";
+    const sentryAuthToken = process.env.SENTRY_AUTH_TOKEN || "";
 
     const isSentryConfigured = Boolean(
         sentryOrg && sentryProject && sentryRelease && sentryAuthToken,
@@ -45,6 +38,8 @@ export default defineConfig(({ mode }) => {
     ];
 
     if (isSentryConfigured) {
+        console.log("Sentry is configured. Will upload source maps on build.");
+        
         plugins.push(
             sentryVitePlugin({
                 org: sentryOrg,
