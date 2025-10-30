6f9a38f05947dd4995d4e0973f76ae1d3c6a7660
5kif4a <theololo057@gmail.com>
2025-10-30 20:58:40 +0500

    feat: enhance UserAvatar component to support bot indicator with badge
    Change-Id: Ie30a12ecb8f5668ab090150eedec9c95d47697dc

--- frontend/src/shared/ui/user_avatar.tsx
+++ frontend/src/shared/ui/user_avatar.tsx
@@ -1,8 +1,31 @@
-import { Avatar } from "@mui/material";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+import { Avatar, Badge, Stack } from "@mui/material";
 import { FC } from "react";
 
-const UserAvatar: FC<{ src: string; size?: number }> = ({ src, size = 24 }) => (
-    <Avatar sx={{ width: size, height: size }} src={src} />
+const UserAvatar: FC<{ src: string; size?: number; isBot?: boolean }> = ({
+    src,
+    size = 24,
+    isBot = false,
+}) => (
+    <Badge
+        badgeContent={
+            isBot ? (
+                <Stack
+                    borderRadius="50%"
+                    bgcolor="info.dark"
+                    p="1px"
+                    border={1}
+                    borderColor="common.white"
+                >
+                    <SmartToyIcon sx={{ fontSize: size / 2 }} />
+                </Stack>
+            ) : null
+        }
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        overlap="circular"
+    >
+        <Avatar sx={{ width: size, height: size }} src={src} />
+    </Badge>
 );
 
 export { UserAvatar };
